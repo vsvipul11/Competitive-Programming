8fdfb8b6a28ea0d4de83017931e7dab27fdae28d
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-04-03 00:19:46 +0530

    Create possible_victory.cpp

---
+++ Codechef/March_starters_2021/possible_victory.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int r,o,c;
+	cin>>r>>o>>c;
+	int x = (20-o)*36;
+	if(x+c>r)
+	{
+	    cout<<"YES"<<endl;
+	}
+	else
+	{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}

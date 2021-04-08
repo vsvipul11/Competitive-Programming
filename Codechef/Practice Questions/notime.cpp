d831ae0587de0cfddca333960d95389c86e10119
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-04-08 23:32:02 +0530

    Create notime.cpp

---
+++ Codechef/Practice Questions/notime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,h,x;
+	cin>>n>>h>>x;
+	int a[n];
+	bool flag = 0;
+	for(int i = 0 ; i< n ;i++){
+	    cin>>a[i];
+	}
+	
+	for(int i = 0 ; i<n ; i++){
+	    if(a[i]+ x>=h){
+	        cout<<"Yes";
+	        flag = 1;
+	        break;
+	    }
+	}
+	if(flag==0){
+	    cout<<"No";
+	}
+	return 0;
+}

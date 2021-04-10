3facb4b2570e6c0ee653f01344af127f3808633a
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-04-10 23:37:18 +0530

    Update luckyfr.cpp

--- Codechef/Practice Questions/luckyfr.cpp
+++ Codechef/Practice Questions/luckyfr.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+using namespace std;
 
+int solve(){
+    int n;
+    cin>>n;
+    int count=0;
+    while(n){
+        int t=n%10;
+        if(t==4){
+          count++;
+        }
+        n/=10;
+    }
+    cout<<count<<'\n';
+    }
+
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        solve();
+    }
+    
+}

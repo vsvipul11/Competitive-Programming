1d05d3a58714b8309759ee01692a4b3f082b4c03
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-04-18 22:57:48 +0530

    Update chocolatemonger.py

--- Codechef/April-challenge2021/chocolatemonger.py
+++ Codechef/April-challenge2021/chocolatemonger.py
@@ -1,1 +1,10 @@
+for _ in range(int(input())):
+  n,x = list(map(int,input().split()))
+  arr = list(map(int,input().split()))
+  u = len(set(arr))
+  r = n-u
+  x-=r
+  if x>0:
+    u-=x
+  print(u)# cook your dish here
 
